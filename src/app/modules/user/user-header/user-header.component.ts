4c1e8e286ec59d7e317a1ad99de6cc94f1cd0ac4
owolabi <owolabi.timothy@longbridgetech.com>
2019-03-17 09:22:22 +0100

    updated user-profile module..2

--- src/app/modules/user/user-header/user-header.component.ts
+++ src/app/modules/user/user-header/user-header.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-user-header',
   templateUrl: './user-header.component.html'
 })
 export class UserHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
+  logout(){
+    if(confirm('Are you sure you want to logout')){
+      this.router.navigate(['home',{}]);
+    }
+  }
 }
