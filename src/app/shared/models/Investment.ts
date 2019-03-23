ad34ff281f88bac287a97f0a554ebf0494fa4020
owolabi <owolabi.timothy@longbridgetech.com>
2019-03-23 09:59:03 +0100

    completed investment creation and view of pools..3

--- src/app/shared/models/Investment.ts
+++ src/app/shared/models/Investment.ts
@@ -3,12 +3,13 @@ export class Investment extends BasicModel{
   title?: string;
   description?: string;
   category_id?: number;
-  max_num_of_slots?: string;
+  max_num_of_slots?: number;
   duration?: string;
   expected_return_period?: string;
   investment_amount?: string;
   opening_date?: string;
   closing_date?: string;
   expected_return_amount?: string;
+  num_of_pools_taken?:number;
   investment_image?:string;
 }
