f833253ec1ea2828c148ea4439d035d4fa0f9487
Falua Temitope Oyewole <emmat0616@gmail.com>
2019-05-31 23:46:57 +0100

    dashboard style corre

--- src/app/core/datasharer/datasharer.service.ts
+++ src/app/core/datasharer/datasharer.service.ts
@@ -1,50 +1,50 @@
-// import { Injectable } from '@angular/core';
-// import {BehaviorSubject} from 'rxjs';
-// import {UserSession} from '../../shared/models/UserSession';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class DatasharerService {
-//   private session = new BehaviorSubject<any>(new UserSession());
-//   private inProfileView = new BehaviorSubject<boolean>(false);
-//   userSession = this.session.asObservable();
-//   profileViewIsActive = this.inProfileView.asObservable();
-//   constructor() { }
-
-  
-//   setSession(userSession: UserSession) {
-//     this.session.next(userSession)
-//   }
-
-//   getSession(){
-//     return this.userSession;
-//   }
-
-//   setInProfileView(profileViewIsActive:boolean){
-//     this.inProfileView.next(profileViewIsActive)
-//   }
-
-//   getInProfileView(){
-//     return this.profileViewIsActive;
-//   }
-
-
-// }
-
 import { Injectable } from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
+import {UserSession} from '../../shared/models/UserSession';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatasharerService {
-  private isLoggedIn = new BehaviorSubject<boolean>(false);
-  loggedIn = this.isLoggedIn.asObservable();
-  constructor(
-    ) {}
+  private session = new BehaviorSubject<any>(new UserSession());
+  private inProfileView = new BehaviorSubject<boolean>(false);
+  userSession = this.session.asObservable();
+  profileViewIsActive = this.inProfileView.asObservable();
+  constructor() { }
 
-  setIsLoggedIn(loggedIn:boolean){
-    this.isLoggedIn.next(loggedIn);
+  
+  setSession(userSession: UserSession) {
+    this.session.next(userSession)
   }
+
+  getSession(){
+    return this.userSession;
+  }
+
+  setInProfileView(profileViewIsActive:boolean){
+    this.inProfileView.next(profileViewIsActive)
+  }
+
+  getInProfileView(){
+    return this.profileViewIsActive;
+  }
+
+
 }
+
+// import { Injectable } from '@angular/core';
+// import {BehaviorSubject} from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class DatasharerService {
+//   private isLoggedIn = new BehaviorSubject<boolean>(false);
+//   loggedIn = this.isLoggedIn.asObservable();
+//   constructor(
+//     ) {}
+
+//   setIsLoggedIn(loggedIn:boolean){
+//     this.isLoggedIn.next(loggedIn);
+//   }
+// }
