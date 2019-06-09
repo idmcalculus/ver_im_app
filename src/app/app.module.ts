ecc61945c6f54ddde9136fa88bb7cd0205769cc5
Emmanuel <macbookpro2015@MacBooks-MacBook-Pro.local>
2019-06-09 23:10:04 +0100

    set up google auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {HomeModule} from './modules/home/home.module';
-import {UserModule} from './modules/user/user.module';
-import {AdminModule} from './modules/admin/admin.module';
-import {InvestmentModule} from './modules/investment/investment.module';
-import {CareerModule} from './modules/career/career.module';
+import { HomeModule } from './modules/home/home.module';
+import { UserModule } from './modules/user/user.module';
+import { AdminModule } from './modules/admin/admin.module';
+import { InvestmentModule } from './modules/investment/investment.module';
+import { CareerModule } from './modules/career/career.module';
 import { AppRoutingModule } from './app-routing.module';
 // import { Angular2SocialLoginModule } from "angular2-social-login";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
- 
+
 import { ToastrModule } from 'ngx-toastr';
 
-import { HttpClientModule }    from '@angular/common/http';
-import {Angular2PromiseButtonModule} from 'angular2-promise-buttons/dist';
+import { HttpClientModule } from '@angular/common/http';
+import { Angular2PromiseButtonModule } from 'angular2-promise-buttons/dist';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
@@ -23,21 +23,25 @@ import { SignUpComponent } from './shared/components/sign-up/sign-up.component';
 import { ServicesComponent } from './modules/services/services.component';
 import { AboutUsComponent } from './modules/about-us/about-us.component';
 import { ForgotPasswordComponent } from './shared/components/forgot-password/forgot-password.component';
-import {VerifyUserComponent} from './shared/components/verify-user/verify-user.component';
+import { VerifyUserComponent } from './shared/components/verify-user/verify-user.component';
 
-import {AppAuthService} from './core/auth/auth.service';
-import {RouterGaurdService} from './core/router-gaurd/router-gaurd';
-import {HttpService} from  './core/http/httpservice.service';
+import { AppAuthService } from './core/auth/auth.service';
+import { RouterGaurdService } from './core/router-gaurd/router-gaurd';
+import { HttpService } from './core/http/httpservice.service';
 import { CookieService } from 'ngx-cookie-service';
 import { ResetPasswordComponent } from './shared/components/reset-password/reset-password.component';
-import {httpInterceptorProviders} from './core/auth/index';
+import { httpInterceptorProviders } from './core/auth/index';
 import { AdminProfileComponent } from './modules/admin/admin-profile/admin-profile.component';
-import {TableComponent} from './shared/components/table/table.component';
+import { TableComponent } from './shared/components/table/table.component';
 import { CloudinaryService } from './shared/services/cloudinary.service';
-import {DynamicScriptLoaderService} from './shared/services/dynamic-script-loader.service';
+import { DynamicScriptLoaderService } from './shared/services/dynamic-script-loader.service';
 import { SocialLogin } from './shared/services/social-login-services';
 import { HowitworksComponent } from './pages/howitworks/howitworks.component';
 import { FaqComponent } from './pages/faq/faq.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 // import { ApplicantDetailsComponent } from './modules/career/applicant-details/applicant-details.component';
 
 
@@ -55,63 +59,65 @@ import { FaqComponent } from './pages/faq/faq.component';
 // };
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    PageNotFoundComponent,
-    SignInComponent,
-    SignUpComponent,
-    VerifyUserComponent,
-    ServicesComponent,
-    AboutUsComponent,
-    ForgotPasswordComponent,
-    ResetPasswordComponent,
-    AdminProfileComponent,
-    TableComponent,
-    HowitworksComponent,
-    FaqComponent,
-    // ApplicantDetailsComponent
+    declarations: [
+        AppComponent,
+        FooterComponent,
+        PageNotFoundComponent,
+        SignInComponent,
+        SignUpComponent,
+        VerifyUserComponent,
+        ServicesComponent,
+        AboutUsComponent,
+        ForgotPasswordComponent,
+        ResetPasswordComponent,
+        AdminProfileComponent,
+        TableComponent,
+        HowitworksComponent,
+        FaqComponent,
+        // ApplicantDetailsComponent
 
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    InvestmentModule,
-    HomeModule,
-    UserModule,
-    AdminModule,
-    CareerModule,
-    AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule, // required animations module
-    ToastrModule.forRoot({
-      timeOut: 5000,
-      positionClass: 'toast-top-right',
-      preventDuplicates: true,
-    }),
-    // Angular2SocialLoginModule,
-    Angular2PromiseButtonModule.forRoot({
-      spinnerTpl: '<span class="btn-spinner"></span>',
-      disableBtn: true,
-      btnLoadingClass: 'is-loading',
-      handleCurrentBtnOnly: false,
-    })
-    // HttpClientInMemoryWebApiModule.forRoot(
-    //   InMemoryDataService, { dataEncapsulation: false }
-    // )
-  ],
-  providers: [
-    RouterGaurdService,
-    HttpService,
-    AppAuthService,
-    CookieService,
-    httpInterceptorProviders,
-    CloudinaryService,
-    DynamicScriptLoaderService,
-    SocialLogin
-  ],
-  exports:[],
-  bootstrap: [AppComponent]
+    ],
+    imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAuthModule,
+        BrowserModule,
+        FormsModule,
+        InvestmentModule,
+        HomeModule,
+        UserModule,
+        AdminModule,
+        CareerModule,
+        AppRoutingModule,
+        HttpClientModule,
+        BrowserAnimationsModule, // required animations module
+        ToastrModule.forRoot({
+            timeOut: 5000,
+            positionClass: 'toast-top-right',
+            preventDuplicates: true,
+        }),
+        // Angular2SocialLoginModule,
+        Angular2PromiseButtonModule.forRoot({
+            spinnerTpl: '<span class="btn-spinner"></span>',
+            disableBtn: true,
+            btnLoadingClass: 'is-loading',
+            handleCurrentBtnOnly: false,
+        })
+        // HttpClientInMemoryWebApiModule.forRoot(
+        //   InMemoryDataService, { dataEncapsulation: false }
+        // )
+    ],
+    providers: [
+        RouterGaurdService,
+        HttpService,
+        AppAuthService,
+        CookieService,
+        httpInterceptorProviders,
+        CloudinaryService,
+        DynamicScriptLoaderService,
+        SocialLogin
+    ],
+    exports: [],
+    bootstrap: [AppComponent]
 })
 
 
