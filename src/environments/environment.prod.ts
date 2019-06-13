d4673453d7c0df51d376174ec97bcdd9baba587c
Parlegrandpa <adebiyipaul1@gmail.com>
2019-06-13 09:59:33 +0100

    created environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase: {
+        apiKey: 'AIzaSyCrV-NTMuNgmdeFH7HIim8Lq6fkdAUE49g',
+        authDomain: 'versa-d18a7.firebaseapp.com',
+        databaseURL: 'https://versa-d18a7.firebaseio.com',
+        projectId: 'versa-d18a7',
+        storageBucket: 'versa-d18a7.appspot.com',
+        messagingSenderId: '839682288435',
+        appId: '1:839682288435:web:d97490f5a3851bd2'
+    }
 };
