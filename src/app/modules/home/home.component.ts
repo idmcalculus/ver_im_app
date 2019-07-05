059e09247b939c40d32d780561a1bfa23d9b2978
Falua Temitope Oyewole <emmat0616@gmail.com>
2019-07-05 19:18:24 +0100

    team added

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,42 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AppAuthService} from './../../core/auth/auth.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { SocialLogin } from '../../shared/services/social-login-services';
+import {ActivatedRoute, Router} from '@angular/router';
+import {SocialLogin} from '../../shared/services/social-login-services';
+
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html'
+    selector: 'app-home',
+    templateUrl: './home.component.html'
 })
 export class HomeComponent {
 
-  constructor(
-    private route:ActivatedRoute,
-    private router:Router,
-    private authService: AppAuthService,
-    private socialAuth:SocialLogin
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private authService: AppAuthService,
+        private socialAuth: SocialLogin
     ) {
-        this.route.queryParams.subscribe(resp=>{
-          var authCode = resp.code;
-          var operation = localStorage.getItem('social_auth_opr');
-          if(authCode){
-            if(authCode.length > 10){
-              this.socialAuth.socialAuth('linkedin',authCode,operation)
-            }else{
-              this.socialAuth.socialAuth('yahoo',authCode,operation)
+        this.route.queryParams.subscribe(resp => {
+            var authCode = resp.code;
+            var operation = localStorage.getItem('social_auth_opr');
+            if (authCode) {
+                if (authCode.length > 10) {
+                    this.socialAuth.socialAuth('linkedin', authCode, operation);
+                } else {
+                    this.socialAuth.socialAuth('yahoo', authCode, operation);
+                }
             }
-          }
-        })
-   }
-
-
-   ngOnInit(){
-    this.authService.setInHomeView(true);
-    }
-
-    ngOnDestroy(){
-      this.authService.setInHomeView(false);
+        });
     }
 
 
-    
+    ngOnInit() {
+        this.authService.setInHomeView(true);
+    }
 
+    ngOnDestroy() {
+        this.authService.setInHomeView(false);
+    }
 }
