65934d0531322e7533f13cb24ff72fafe74e3376
Parlegrandpa <adebiyipaul1@gmail.com>
2019-07-12 13:58:40 +0100

    added versa team

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,90 +9,90 @@ export const environment = {
       messagingSenderId: '839682288435',
       appId: '1:839682288435:web:d97490f5a3851bd2'
   },
-//   "app-live-url":"https://live.adebiyipaul.com/api",
-//   "server_services_base":"https://versa.ng",
-//   "cloudinary": {
-//       url:"https://api.cloudinary.com",
-//       uploadUrl:"v1_1/crystalbit-technology/image/upload",
-//       upload_preset: "vprr7erl",
-//       apiKey: "YOUR API KEY",
-//       databaseURL: "YOUR DATABASE URL",
-//       projectId: "YOUR PROJECT ID",
-//       storageBucket: "YOUR STORAGE BUCKET",
-//       messagingSenderId: "YOUR MESSAGING SENDER ID"
-//       },
-//   "google":{
-//       scope:"profile email",
-//       clientid:"104742513131-r6pnjt53en8akmt4pqt9d3i5ia5iln8a.apps.googleusercontent.com",
-//       cookiepolicy: 'single_host_origin'
-//   },
-//   "yahoo":{
-//       base_url:"https://api.login.yahoo.com",
-//       auth_code_path:"oauth2/request_auth",
-//       redirect_uri:"https://versa.ng",
-//       access_token_path:"oauth2/get_token",
-//       scope:"profile",
-//       clientid:"dj0yJmk9Q01TdTRFbHZNVjJjJmQ9WVdrOVdVdFRORE5oTkdzbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWYx",
-//       secretkey:"60cd5ca9a18f67c3bf418f98218a6fc177d4ac6c",
-//       profil_base_url:"https://social.yahooapis.com"
+  "app-live-url":"https://live.adebiyipaul.com/api",
+  "server_services_base":"https://versa.ng",
+  "cloudinary": {
+      url:"https://api.cloudinary.com",
+      uploadUrl:"v1_1/crystalbit-technology/image/upload",
+      upload_preset: "vprr7erl",
+      apiKey: "YOUR API KEY",
+      databaseURL: "YOUR DATABASE URL",
+      projectId: "YOUR PROJECT ID",
+      storageBucket: "YOUR STORAGE BUCKET",
+      messagingSenderId: "YOUR MESSAGING SENDER ID"
+      },
+  "google":{
+      scope:"profile email",
+      clientid:"104742513131-r6pnjt53en8akmt4pqt9d3i5ia5iln8a.apps.googleusercontent.com",
+      cookiepolicy: 'single_host_origin'
+  },
+  "yahoo":{
+      base_url:"https://api.login.yahoo.com",
+      auth_code_path:"oauth2/request_auth",
+      redirect_uri:"https://versa.ng",
+      access_token_path:"oauth2/get_token",
+      scope:"profile",
+      clientid:"dj0yJmk9Q01TdTRFbHZNVjJjJmQ9WVdrOVdVdFRORE5oTkdzbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWYx",
+      secretkey:"60cd5ca9a18f67c3bf418f98218a6fc177d4ac6c",
+      profil_base_url:"https://social.yahooapis.com"
       
       
-//   },
-//   "linkedin":{
-//       base_url:"https://www.linkedin.com",
-//       auth_code_path:"oauth/v2/authorization",
-//       redirect_uri:"https://versa.ng",
-//       access_token_path:"oauth/v2/accessToken",
-//       scope:"r_liteprofile%20r_emailaddress%20w_member_social",
-//       clientid:"8604gpqc8lasyf",
-//       secretkey:'BhWw3I9ufi6KLr0k',
-//       profile_email:"v2/me",
-//       response_type:"code",
-//       grant_type:"authorization_code",
-//       state:"fooobar",
-//       profile_base_url:'https://api.linkedin.com'
-//   }
+  },
+  "linkedin":{
+      base_url:"https://www.linkedin.com",
+      auth_code_path:"oauth/v2/authorization",
+      redirect_uri:"https://versa.ng",
+      access_token_path:"oauth/v2/accessToken",
+      scope:"r_liteprofile%20r_emailaddress%20w_member_social",
+      clientid:"8604gpqc8lasyf",
+      secretkey:'BhWw3I9ufi6KLr0k',
+      profile_email:"v2/me",
+      response_type:"code",
+      grant_type:"authorization_code",
+      state:"fooobar",
+      profile_base_url:'https://api.linkedin.com'
+  }
 
 
-    "app-live-url":"https://versabackend.adebiyipaul.com/api",
-    "server_services_base":"https://versa-ims.herokuapp.com",
-    "cloudinary": {
-        url:"https://api.cloudinary.com",
-        uploadUrl:"v1_1/crystalbit-technology/image/upload",
-        upload_preset: "vprr7erl",
-        apiKey: "YOUR API KEY",
-        databaseURL: "YOUR DATABASE URL",
-        projectId: "YOUR PROJECT ID",
-        storageBucket: "YOUR STORAGE BUCKET",
-        messagingSenderId: "YOUR MESSAGING SENDER ID"
-        },
-    "google":{
-        scope:"profile email",
-        clientid:"104742513131-r6pnjt53en8akmt4pqt9d3i5ia5iln8a.apps.googleusercontent.com",
-        cookiepolicy: 'single_host_origin'
-    },
-    "yahoo":{
-        base_url:"https://api.login.yahoo.com",
-        auth_code_path:"oauth2/request_auth",
-        redirect_uri:"http://versa-ims.herokuapp.com",
-        access_token_path:"oauth2/get_token",
-        scope:"profile",
-        clientid:"dj0yJmk9eHNIendLV2NJU2gwJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTg5",
-        secretkey:"74822b31d3242615561b84a5ece6020b9ef5d9a3",
-        profil_base_url:"https://social.yahooapis.com"   
-    },
-    "linkedin":{
-        base_url:"https://www.linkedin.com",
-        auth_code_path:"oauth/v2/authorization",
-        redirect_uri:"http://versa-ims.herokuapp.com",
-        access_token_path:"oauth/v2/accessToken",
-        scope:"r_liteprofile%20r_emailaddress%20w_member_social",
-        clientid:"77pv3mo63oyixv",
-        secretkey:'WXSct7I6waMjYI06',
-        profile_email:"v2/me",
-        response_type:"code",
-        grant_type:"authorization_code",
-        state:"fooobar",
-        profile_base_url:'https://api.linkedin.com'
-    }
+    // "app-live-url":"https://versabackend.adebiyipaul.com/api",
+    // "server_services_base":"https://versa-ims.herokuapp.com",
+    // "cloudinary": {
+    //     url:"https://api.cloudinary.com",
+    //     uploadUrl:"v1_1/crystalbit-technology/image/upload",
+    //     upload_preset: "vprr7erl",
+    //     apiKey: "YOUR API KEY",
+    //     databaseURL: "YOUR DATABASE URL",
+    //     projectId: "YOUR PROJECT ID",
+    //     storageBucket: "YOUR STORAGE BUCKET",
+    //     messagingSenderId: "YOUR MESSAGING SENDER ID"
+    //     },
+    // "google":{
+    //     scope:"profile email",
+    //     clientid:"104742513131-r6pnjt53en8akmt4pqt9d3i5ia5iln8a.apps.googleusercontent.com",
+    //     cookiepolicy: 'single_host_origin'
+    // },
+    // "yahoo":{
+    //     base_url:"https://api.login.yahoo.com",
+    //     auth_code_path:"oauth2/request_auth",
+    //     redirect_uri:"http://versa-ims.herokuapp.com",
+    //     access_token_path:"oauth2/get_token",
+    //     scope:"profile",
+    //     clientid:"dj0yJmk9eHNIendLV2NJU2gwJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTg5",
+    //     secretkey:"74822b31d3242615561b84a5ece6020b9ef5d9a3",
+    //     profil_base_url:"https://social.yahooapis.com"
+    // },
+    // "linkedin":{
+    //     base_url:"https://www.linkedin.com",
+    //     auth_code_path:"oauth/v2/authorization",
+    //     redirect_uri:"http://versa-ims.herokuapp.com",
+    //     access_token_path:"oauth/v2/accessToken",
+    //     scope:"r_liteprofile%20r_emailaddress%20w_member_social",
+    //     clientid:"77pv3mo63oyixv",
+    //     secretkey:'WXSct7I6waMjYI06',
+    //     profile_email:"v2/me",
+    //     response_type:"code",
+    //     grant_type:"authorization_code",
+    //     state:"fooobar",
+    //     profile_base_url:'https://api.linkedin.com'
+    // }
 };
