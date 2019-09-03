ead5c76921e27d349fcf943118b1b92f94b1afff
Falua Temitope Oyewole <emmat0616@gmail.com>
2019-09-03 09:09:42 +0100

    invest details

--- src/app/modules/investment/investment.component.ts
+++ src/app/modules/investment/investment.component.ts
@@ -1,99 +1,95 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 import {InvestmentService} from './investment.service';
-import { Transaction } from 'src/app/shared/models/Transaction';
+import {Transaction} from 'src/app/shared/models/Transaction';
 
 
-
-let category="0"
-let allInvestments=[]
+let category = '0';
+let allInvestments = [];
 
 @Component({
-  selector: 'app-investment',
-  templateUrl: './investment.component.html'
+    selector: 'app-investment',
+    templateUrl: './investment.component.html'
 })
-
 
 
 export class InvestmentComponent implements OnInit {
 
 
+    isLoading: boolean = true;
+    investments: any = [];
+    categories: any = [];
+    selectedCategory: string = '0';
 
-  isLoading:boolean=true;
-  investments:any=[];
-  categories:any=[];
-  selectedCategory:string='0';
+    transaction: Transaction;
 
-  transaction:Transaction;
-  constructor(
-    private routes:Router,
-    private investmentService:InvestmentService,
-    private activatedRoute:ActivatedRoute) {
-    this.getCategories();
-   }
-
-  ngOnInit() {
-    this.getInvestments();
-    
-    
-  }
-
-
-  getInvestments(){
-    
-    this.investmentService.getInvestments(true).subscribe(investments=>{
-      var investmentArray=[];
-      if(investments){
-        investmentArray = investments.success.Data
-        var cnt = 0;
-        investmentArray.forEach(element => {
-          if(element.is_investment_started=='0' && element.is_investment_ended=='0'){
-            this.investments[cnt] = element;
-            cnt++;
-          }
-        });
-      }
-      allInvestments = this.investments;
-      this.isLoading = false;
-      
-      var categoryName = this.activatedRoute.snapshot.params['category'];
-      if(categoryName){
-        var category = this.categories.filter(a1=>{
-          return a1.category_name.trim() == categoryName.trim();
-        })
-        if(category && category.length > 0){
-          this.selectedCategory = category[0].id
-          this.filterInvestments();
-        }
-      }
-    })
-  }
-
-
-  getCategories(){
-    this.investmentService.getCategories().subscribe(categories=>{
-      if(categories && categories.success){
-        this.categories = categories.success.Data;
-      }
-    })
-  }
-
-  filterInvestments(){
-    category = this.selectedCategory;
-    if(category=="0"){
-      this.investments = allInvestments;
-    }else{
-      this.investments = allInvestments.filter(a1=>{
-        return a1.category_id == category;
-      })
+    constructor(
+        private routes: Router,
+        private investmentService: InvestmentService,
+        private activatedRoute: ActivatedRoute) {
+        this.getCategories();
     }
-  }
 
-  filterInvestmentsByName(categoryName){
-    this.investments = allInvestments.filter(a1=>{
-      return a1.category_name == categoryName;
-    })
-  }
+    ngOnInit() {
+        this.getInvestments();
+    }
+
+
+    getInvestments() {
+
+        this.investmentService.getInvestments(true).subscribe(investments => {
+            var investmentArray = [];
+            if (investments) {
+                investmentArray = investments.success.Data;
+                var cnt = 0;
+                investmentArray.forEach(element => {
+                    if (element.is_investment_started === '0' && element.is_investment_ended === '0') {
+                        this.investments[cnt] = element;
+                        cnt++;
+                    }
+                });
+            }
+            allInvestments = this.investments;
+            this.isLoading = false;
+
+            var categoryName = this.activatedRoute.snapshot.params['category'];
+            if (categoryName) {
+                var category = this.categories.filter(a1 => {
+                    return a1.category_name.trim() == categoryName.trim();
+                });
+                if (category && category.length > 0) {
+                    this.selectedCategory = category[0].id;
+                    this.filterInvestments();
+                }
+            }
+        });
+    }
+
+
+    getCategories() {
+        this.investmentService.getCategories().subscribe(categories => {
+            if (categories && categories.success) {
+                this.categories = categories.success.Data;
+            }
+        });
+    }
+
+    filterInvestments() {
+        category = this.selectedCategory;
+        if (category === '0') {
+            this.investments = allInvestments;
+        } else {
+            this.investments = allInvestments.filter(a1 => {
+                return a1.category_id === category;
+            });
+        }
+    }
+
+    filterInvestmentsByName(categoryName) {
+        this.investments = allInvestments.filter(a1 => {
+            return a1.category_name === categoryName;
+        });
+    }
 
 
 }
