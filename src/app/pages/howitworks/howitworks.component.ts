c62027da0c2b7607524d31807c0c2574381baea2
kabiribraheem <ibraheemkabir9@gmail.com git config --global user.name kaniribraheem git config --global user.email ibraheemkabir9@gmail.com>
2019-09-16 22:22:05 +0100

    faq  and about us pages

--- src/app/pages/howitworks/howitworks.component.ts
+++ src/app/pages/howitworks/howitworks.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-howitworks',
-  templateUrl: './howitworks.component.html'
+  templateUrl: './howitworks.component.html',
+  styleUrls: ['./faq.scss']
 })
 export class HowitworksComponent implements OnInit {
 
