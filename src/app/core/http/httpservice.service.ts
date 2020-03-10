2013455fbe15e624a9438e420f48aebab1d632de
Damilola Michael Ige <idm.calculus@gmail.com>
2020-03-10 12:47:47 +0100

    updates manage-category

--- src/app/core/http/httpservice.service.ts
+++ src/app/core/http/httpservice.service.ts
@@ -3,7 +3,7 @@ import {  HttpHeaders, HttpClient  } from '@angular/common/http';
 import { Observable, of, observable, throwError } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
-import {environment as appConfig} from '../../../environments/environment'
+import {environment as appConfig} from '../../../environments/environment';
 
 
 const httpOptions = {
@@ -15,10 +15,10 @@ const httpOptions = {
 @Injectable({ providedIn: 'root' })
 export class HttpService {
   headers: any;
-  requestOption:any;
-  response:{};
-  baseURL:string =appConfig["app-live-url"]
-  
+  requestOption: any;
+  response: {};
+  baseURL: string = appConfig['app-live-url'];
+
 
     constructor(
         private http: HttpClient,
@@ -37,11 +37,11 @@ export class HttpService {
         );
     }
 
-    postRequest(api: string, data: any,showMessages?:boolean, httpHeaderOptions?: {headers: HttpHeaders}): Observable<any>  {
+    postRequest(api: string, data: any, showMessages?: boolean, httpHeaderOptions?: {headers: HttpHeaders}): Observable<any>  {
         const opts = httpHeaderOptions ? httpHeaderOptions : httpOptions;
         return this.http.post<any>(`${this.baseURL}/${api}`, data, opts)
         .pipe(
-            tap(resp => this.log(resp,showMessages)),
+            tap(resp => this.log(resp, showMessages)),
             catchError(
                 this.handleError<any>(api, {})
             )
@@ -64,40 +64,40 @@ export class HttpService {
 
     private handleError<T>(operation = 'operation', result?: T) {
         return (error: any): Observable<T> => {
-            JSON.stringify(error)
-            if(error.status==0){
-                console.log("Error occurred is:: "+JSON.stringify(error))
-                this.toastrService.error(`Error occurred connecting to services`)
-            }else{
-                if(error.error && error.error.errors){
-                    var obj = error.error.errors;
+            JSON.stringify(error);
+            if (error.status == 0) {
+                console.log('Error occurred is:: '+ JSON.stringify(error));
+                this.toastrService.error(`Error occurred connecting to services`);
+            } else {
+                if (error.error && error.error.errors) {
+                    let obj = error.error.errors;
                     obj = Object.values(obj);
-                    for(var i=0;i<obj.length;i++){
-                        this.toastrService.error(obj[i])
+                    for (let i = 0; i < obj.length; i++) {
+                        this.toastrService.error(obj[i]);
                     }
-                }else if(error.error && error.error.error){
-                    if(error.error.error.message){
-                        this.toastrService.error(error.error.error.message)
-                    }else if(error.error.error.Message){
-                        this.toastrService.error(error.error.error.Message)
-                    } 
-                }else{
-                    
+                } else if (error.error && error.error.error) {
+                    if (error.error.error.message) {
+                        this.toastrService.error(error.error.error.message);
+                    } else if (error.error.error.Message) {
+                        this.toastrService.error(error.error.error.Message);
+                    }
+                } else {
+
                 }
             }
             return of(result as T);
         };
     }
 
-    private log(message: any,alertMessage?:boolean) {
-        if(alertMessage){
-            if(message.success.Message){
+    private log(message: any, alertMessage?: boolean) {
+        if (alertMessage) {
+            if (message.success.Message) {
                 this.toastrService.success(message.success.Message);
-            }else if(message.success.message){
+            } else if (message.success.message) {
                 this.toastrService.success(message.success.message);
             }
-            
+
         }
-        
+
     }
 }
--- src/app/modules/admin/manage-category/manage-category.component.ts
+++ src/app/modules/admin/manage-category/manage-category.component.ts
@@ -9,62 +9,62 @@ import { Category } from 'src/app/shared/models/Category';
 })
 export class ManageCategoryComponent implements OnInit {
 
-  buttonText='Add Category'
-  updateButtonText='update'
-  inEditMode=false;
-  isLoading:boolean=true;
-  category:Category={category_name:''};
-  categories=[]
-  constructor(private investmentService:InvestmentService) { 
-    this.getCategories()
+  buttonText = 'Add Category';
+  updateButtonText = 'update';
+  inEditMode = false;
+  isLoading = true;
+  category: Category = {category_name: ''};
+  categories = [];
+  constructor(private investmentService: InvestmentService) {
+    this.getCategories();
   }
 
   ngOnInit() {
   }
 
-  addCategory(){
-    if(this.category.category_name){
-        this.buttonText = 'Submitting'
-        this.investmentService.addCategory(this.category).subscribe(resp=>{
-          if(resp && resp.success){
+  addCategory() {
+    if (this.category.category_name) {
+        this.buttonText = 'Submitting';
+        this.investmentService.addCategory(this.category).subscribe(resp => {
+          if (resp && resp.success) {
             // alert(resp.success.Message);
-            this.getCategories()
+            this.getCategories();
           }
-          this.buttonText = 'Add Category'
-        })
-    }    
-  }
-
-  updateCategory(category:Category){
-    this.updateButtonText = 'updating';
-    this.investmentService.updateCategory(category).subscribe(resp=>{
-      if(resp && resp.success){
-        category.inEditMode = !category.inEditMode;
-        // alert(resp.success.Message);
-        this.updateButtonText = 'update'
-      }
-    })
-  }
-
-  deleteCategory(category){
-    if(confirm(`Confirm Deletion of categoory ${category.category_name}`)){
-      this.investmentService.deleteCategory(category).subscribe(resp=>{
-        if(resp && resp.success){
-          // alert(resp.success.Message);
-          this.getCategories()
-        }
-      })
+          this.buttonText = 'Add Category';
+        });
     }
   }
 
-  getCategories(){
-    this.isLoading=true;
-    this.investmentService.getCategories().subscribe(resp=>{
-      if(resp && resp.success){
+  updateCategory(category: Category) {
+    this.updateButtonText = 'updating';
+    this.investmentService.updateCategory(category).subscribe(resp => {
+      if (resp && resp.success) {
+        category.inEditMode = !category.inEditMode;
+        // alert(resp.success.Message);
+        this.updateButtonText = 'update';
+      }
+    });
+  }
+
+  deleteCategory(category) {
+    if (confirm(`Confirm Deletion of categoory ${category.category_name}`)) {
+      this.investmentService.deleteCategory(category).subscribe(resp => {
+        if (resp && resp.success) {
+          // alert(resp.success.Message);
+          this.getCategories();
+        }
+      });
+    }
+  }
+
+  getCategories() {
+    this.isLoading = true;
+    this.investmentService.getCategories().subscribe(resp => {
+      if (resp && resp.success) {
         this.categories = resp.success.Data;
       }
-      this.isLoading=false;
-    })
+      this.isLoading = false;
+    });
   }
 
 
