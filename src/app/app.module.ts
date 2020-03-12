4253031abaf2458b1373001276f7dce2a9e82df2
Damilola Michael Ige <idm.calculus@gmail.com>
2020-03-12 17:02:28 +0100

    changes to category-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,6 +42,7 @@ import { SocialLogin } from './shared/services/social-login-services';
 import { HowitworksComponent } from './pages/howitworks/howitworks.component';
 import { FaqComponent } from './pages/faq/faq.component';
 import { ContactusComponent } from './pages/contactus/faq.component';
+import { SearchService } from '../app/shared/services/search.service';
 
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
@@ -126,7 +127,8 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
         httpInterceptorProviders,
         CloudinaryService,
         DynamicScriptLoaderService,
-        SocialLogin
+        SocialLogin,
+        SearchService
     ],
     exports: [],
     bootstrap: [AppComponent]
