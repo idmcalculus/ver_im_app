a3185e6bfddb17e451b753c2934db2f01940edeb
unknown <ibraheemkabir96@gmail.com>
2020-03-16 00:09:58 +0100

    merged in investemnts view and customers

--- src/app/modules/admin/manage-users/list-users/list_users.component.spec.ts
+++ src/app/modules/admin/manage-users/list-users/list_users.component.spec.ts
@@ -1,10 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-<<<<<<< Updated upstream:src/app/modules/admin/manage-users/list-users/list_users.component.spec.ts
 import { ManageUsersComponent } from './list_users.component';
-=======
 import { ManageUsersComponent } from './view_customer.component';
->>>>>>> Stashed changes:src/app/modules/admin/manage-customer/view-customer/view_customer.component.spec.ts
 
 describe('ManageUsersComponent', () => {
   let component: ManageUsersComponent;
