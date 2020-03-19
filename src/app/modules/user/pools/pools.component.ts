038e732cb92ca65a743c1cb88e2066887ecf3caf
samajaloleru <samsonajaloleru@gmail.com>
2020-03-19 10:42:01 +0100

    cleaning up codebase

--- src/app/modules/user/pools/pools.component.ts
+++ src/app/modules/user/pools/pools.component.ts
@@ -15,6 +15,9 @@ export class PoolsComponent implements OnInit {
   pools:Investment[]=[];
   pool:Investment;
   userType:string;
+  masterSelected:boolean;
+  checklist:any;
+  checkedList:any;
 
   constructor(
     private authService:AppAuthService,
@@ -33,12 +36,36 @@ export class PoolsComponent implements OnInit {
         this.userType = 'admin';
         this.getPools();
       }
+      this.masterSelected = false;
+      this.checklist = [this.pool,];
+      this.getCheckedPooList();
       
   }
 
   ngOnInit() {
   }
-
+  
+  checkUncheckAll() {
+    for (var i = 0; i < this.checklist.length; i++) {
+      this.checklist[i] = this.masterSelected;
+    }
+    this.getCheckedPooList();
+  }
+  isAllSelected() {
+    this.masterSelected = this.checklist.every(function(pool:any) {
+        return pool == true;
+      })
+    this.getCheckedPooList();
+  }
+ 
+  getCheckedPooList(){
+    this.checkedList = [];
+    for (var i = 0; i < this.checklist.length; i++) {
+      if(this.checklist[i])
+      this.checkedList.push(this.checklist[i]);
+    }
+    this.checkedList = JSON.stringify(this.checkedList);
+  }
 
   getPools(){
     this.investmentService.getInvestments(false).subscribe(investments=>{
