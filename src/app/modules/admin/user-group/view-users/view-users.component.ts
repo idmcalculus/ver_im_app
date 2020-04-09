341cff04659cc0e56400e0698b82200f9347f6df
Damilola Ige <idm.calculus@gmail.com>
2020-04-09 16:17:04 +0100

    work on view users

--- src/app/modules/admin/user-group/view-users/view-users.component.ts
+++ src/app/modules/admin/user-group/view-users/view-users.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { User } from "src/app/shared/models/user";
+import { AppAuthService } from "src/app/core/auth/auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-view-users',
-  templateUrl: './view-users.component.html',
-  styleUrls: ['./view-users.component.scss']
+    selector: "app-view-users",
+    templateUrl: "./view-users.component.html",
+    styleUrls: ["./view-users.component.scss"],
 })
 export class ViewUsersComponent implements OnInit {
+    isLoading = false;
+    loggedInUser: User;
+    selectedUser: User;
+    userSubscription: Subscription;
 
-  constructor() { }
+    constructor(private authService: AppAuthService) {
+        this.userSubscription = this.authService.currentUser.subscribe(
+            (userInfo) => {
+                if (userInfo) {
+                    this.loggedInUser = userInfo;
+                }
+            }
+        );
+    }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
