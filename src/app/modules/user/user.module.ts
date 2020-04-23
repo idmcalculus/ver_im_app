4d81e0e7751648eb08d480a026566c563f27432c
ibraheemkabir <ibraheemkabir96@gmail.com>
2020-04-23 14:14:30 +0100

    fixed up issues

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -7,7 +7,6 @@ import {Angular2PromiseButtonModule} from 'angular2-promise-buttons/dist';
 
 import {UserRoutingModule} from './user-routing.module';
 import { UserSettingsComponent } from './settings/settings.component';
-import { EditPasswordComponent } from './settings/edit-password/edit_password.component';
 import {FormsModule} from '@angular/forms';
 import { userPoolsComponent } from './user-pool/user-pool.component';
 import { userPoolDetailComponent } from './userpool-detail/userpool-detail.component';
