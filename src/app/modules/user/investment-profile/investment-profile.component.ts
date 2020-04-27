26a0f5aab8c56074ece1a3b09182d2e1b551a123
Damilola Michael Ige <idm.calculus@gmail.com>
2020-04-27 20:54:34 +0100

    enabled pay investors functionality and updated corresponding pool details components

--- src/app/modules/user/investment-profile/investment-profile.component.ts
+++ src/app/modules/user/investment-profile/investment-profile.component.ts
@@ -10,6 +10,7 @@ import { UserService } from '../user.service';
 import { DynamicScriptLoaderService } from 'src/app/shared/services/dynamic-script-loader.service';
 import { ReportService } from 'src/app/shared/components/report/report.service';
 import { Report } from 'src/app/shared/models/Report';
+import { ToastrService } from 'ngx-toastr';
 
 
 
@@ -43,7 +44,7 @@ export class InvestmentProfileComponent implements OnInit {
   period: string;
   returns: string;
   numOfReports: number;
-  latestReport: any;
+  latestReport: Report = {title: '', returned_amount: 0, investment_id: 0, payment_type: '', description: ''};
   reportId = 0;
   returnedAmount: number;
   user_id: string;
@@ -59,7 +60,8 @@ export class InvestmentProfileComponent implements OnInit {
               private authService: AppAuthService,
               private userService: UserService,
               private dynamicScriptLoader: DynamicScriptLoaderService,
-              private reportService: ReportService) {
+              private reportService: ReportService,
+              private toastrService: ToastrService) {
                 this.userSubscription = this.authService.currentUser.subscribe(userInfo => {
                   if (userInfo) {
                     this.loggedInUser = userInfo;
@@ -71,101 +73,43 @@ export class InvestmentProfileComponent implements OnInit {
                   if (!this.poolId) {
                     this.poolId = Number(this.route.snapshot.paramMap.get('id'));
                   }
-                  this.fetchPool(String(this.poolId));
-                  this.investmentService.getInvestment(String(this.poolId)).subscribe(async poolDetails => {
-                    if (poolDetails && poolDetails.success) {
-                      if (poolDetails.success.Data) {
-                        this.pool = await poolDetails.success.Data;
-                        console.log(this.pool);
-                        this.userEmail = this.pool.investment_user[0].user_info[0].email;
-                        this.getUserDashBoard(this.poolId, this.userEmail);
-                      }
-                    }
-                  });
+                });
+                this.isLoading = true;
+                this.investmentService.getInvestment(String(this.poolId)).subscribe(async poolDetails => {
+                  if (poolDetails && poolDetails.success) {
+                      this.pool = await poolDetails.success.Data;
+                      this.numOfReports = this.pool.report.length;
+                      this.latestReport = this.pool.report[0];
+                      this.isLoading = false;
+                  }
                 });
                 this.loadScripts();
                }
 
-  ngOnInit() {
-   /* this.investmentService.getUserInvestments(this.user.email).subscribe(investments => {
-      if (investments.success.Data !== 0) {
-        this.userInvestment = investments.success.Data;
-        console.log(this.userInvestment);
-        this.selectedInvestment = 0;
-        // this.showDetails();
-        this.FilteredInvestment = this.userInvestment.filter((investment: Investment) => investment.is_investment_ended === '0');
-      } else {
-          this.isLoading = true;
-      }
-  });*/
-}
-
-  async fetchPool(poolId: string) {
-    this.isLoading = true;
-    this.investmentService.getInvestment(poolId).subscribe(async poolDetails => {
-      if (poolDetails && poolDetails.success) {
-        if (poolDetails.success.Data) {
-          this.pool = await poolDetails.success.Data;
-          this.isLoading = false;
-        } else {
-          this.router.navigate(['./', {}]);
-        }
-      } else {
-      }
-    });
-  }
-
-  /*showDetails() {
-    if ( this.selectedInvestment <= (this.userInvestment.length - 1) ) {
-        this.investmentInfo = this.userInvestment[this.selectedInvestment];
-        //this.getUserDashBoard();
-        this.selectedInvestment++;
-        return this.selectedInvestment;
-        } else {
-        this.dashBoardData = {number_of_pools: 0, investment_return: [], investment_report: []};
-        this.isLoading = true;
-        }
-  }*/
-
-  getUserDashBoard(investmentId, userEmail) {
-    this.isLoading = true;
-    this.userService.getUserDashBoard(investmentId, userEmail).subscribe(async resp => {
-      if (resp && resp.success) {
-        this.dashBoardData = await resp.success.Data;
-        console.log(this.dashBoardData);
-        this.isLoading = false;
-      } else {
-        this.dashBoardData = {number_of_pools: 0, investment_return: [], investment_report: []};
-      }
-      this.numOfReports = this.dashBoardData.investment_report.length;
-      this.latestReport = this.dashBoardData.investment_report[this.numOfReports - 1];
-      // this.showDetails();
-    });
-  }
+  ngOnInit() {}
 
   payInvestors(report: Report) {
     this.isLoading = true;
-    this.title = this.latestReport.title;
-    this.returnedAmount = this.latestReport.returned_amount;
-    this.description = `Return on Investment Week ${this.numOfReports + 1}`;
-    this.user_id = this.loggedInUser.email;
-    this.investmentId = this.latestReport.investment_id;
-    this.paymentType = this.latestReport.payment_type;
+    report.title = this.pool.investment.title;
+    report.returned_amount = Number(this.pool.investment.expected_return_amount);
+    report.investment_id = this.pool.investment.id;
+    report.payment_type = 'Credit';
+    report.description = `Return on Investment Week ${this.numOfReports + 1}`;
 
     const data = {
       title: report.title,
-      description: this.description,
+      description: report.description,
       returned_amount: report.returned_amount,
       investment_id: report.investment_id,
       payment_type: report.payment_type,
-      report_id: report.id
     };
 
-    this.reportService.updateReport(data).subscribe(resp => {
+    this.reportService.createReport(data).subscribe(resp => {
       if (resp && resp.success) {
-        console.log(resp);
+        this.toastrService.success('Investors were paid successfully');
+        this.isLoading = false;
       }
-      this.isLoading = false;
+      this.location.back();
     });
   }
 
